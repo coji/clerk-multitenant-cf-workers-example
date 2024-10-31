21167645520275d5c052d281fef38666b70a05fd
coji <coji@techtalk.jp>
2024-10-31 22:33:19 +0900

    Refactor Clerk webhook route to use type imports and enhance logging structure

--- app/routes/api.webhooks.clerk/route.ts
+++ app/routes/api.webhooks.clerk/route.ts
@@ -1,25 +1,28 @@
-import {
-  type ActionFunctionArgs,
-  type LoaderFunctionArgs,
-  data,
+import type {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
 } from '@remix-run/cloudflare'
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const json = await request.json()
-  return data({
+  const log = {
     method: request.method,
     headers: request.headers,
     url: request.url,
     json,
-  })
+  }
+  console.dir(log, { depth: null })
+  return Response.json(log)
 }
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const json = await request.json()
-  return data({
+  const log = {
     method: request.method,
     headers: request.headers,
     url: request.url,
     json,
-  })
+  }
+  console.dir(log, { depth: null })
+  return Response.json(log)
 }
