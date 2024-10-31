00c6cbb5cda77fa6072b6e4f4fd415df52b402d5
coji <coji@techtalk.jp>
2024-10-31 22:20:49 +0900

    Refactor API webhook route to use named imports and streamline response structure

--- app/routes/api.webhooks.clerk/route.ts
+++ app/routes/api.webhooks.clerk/route.ts
@@ -1,24 +1,25 @@
-import type {
-  ActionFunctionArgs,
-  LoaderFunctionArgs,
+import {
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+  data,
 } from '@remix-run/cloudflare'
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const json = await request.json()
-  return {
+  return data({
     method: request.method,
     headers: request.headers,
     url: request.url,
     json,
-  }
+  })
 }
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const json = await request.json()
-  return {
+  return data({
     method: request.method,
     headers: request.headers,
     url: request.url,
     json,
-  }
+  })
 }
